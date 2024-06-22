3316077839f3c06d22ce332547b25e34c48136a6
Siddharth Singh <me@shantaram.xyz>
2024-06-22 20:16:30 +0530

    add getSessionChoices

--- src/commands/session/mod.ts
+++ src/commands/session/mod.ts
@@ -1,8 +1,24 @@
 import { Command } from "@/commander";
 import { start } from "./start.ts";
 import { stop } from "./stop.ts";
+import { Session } from "../../utils.ts";
+import * as storage from '../../storage.ts';
+import { deleteSession } from "./delete.ts";
+
+
+export const getSessionChoices = async (id: string) => {
+    const choices: string[] = [];
+    const map: Record<string, Session> = {};
+    for await (const session of storage.getSessions(id)) {
+        const key = session.key[3].toString();
+        choices.push(key);
+        map[key] = session.value;
+    }
+    return { choices, map };
+}
 
 export const session = new Command("session")
     .description("Edit and manage sessions.")
     .addCommand(start)
-    .addCommand(stop);
+    .addCommand(stop)
+    .addCommand(deleteSession);
