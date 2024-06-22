ff29cda218159f5e1d842a5cd74383e2a2dbdc27
Siddharth Singh <me@shantaram.xyz>
2024-06-22 13:44:14 +0530

    replace bare kv interface with EtuStorage

--- src/kv.ts
+++
@@ -1,9 +0,0 @@
-import { join } from "https://deno.land/std@0.204.0/path/join.ts";
-import dir from "https://deno.land/x/dir@1.5.2/mod.ts";
-
-const dataDir = dir('data');
-if (!dataDir) throw new Error("Couldn't find your data directory! Bailing.");
-
-const dataPath = join(dataDir, 'etu');
-await Deno.mkdir(dataPath, { recursive: true });
-export const kv = await Deno.openKv(join(dataPath, 'etu.db'));
---
+++ src/storage.ts
@@ -0,0 +1,44 @@
+import { join } from "https://deno.land/std@0.204.0/path/join.ts";
+import dir from "https://deno.land/x/dir@1.5.2/mod.ts";
+import { Project, Session } from "./utils.ts";
+import { None, Option, Result } from "@/oxide";
+import { Some } from "@/oxide";
+import { ulid } from "@/ulid";
+import { Err } from "@/oxide";
+import { Ok } from "@/oxide";
+
+const dataDir = dir('data');
+if (!dataDir) throw new Error("Couldn't find your data directory! Bailing.");
+
+const dataPath = join(dataDir, 'etu');
+await Deno.mkdir(dataPath, { recursive: true });
+const kv = await Deno.openKv(join(dataPath, 'etu.db'));
+
+export class EtuStorage {
+    static async getDefaultProject(): Promise<Option<string>> {
+        const project = await kv.get<string>(['projects', 'default']);
+        return Option(project.value);
+    }
+
+    static async getProjectById(id: string): Promise<Option<string>> {
+        const project = await kv.get<Project>(['projects', id]);
+        if (project.value) return Some(id);
+        return None;
+    }
+
+    static async setProject(project: Project) {
+        await kv.set(['projects', project.slug], project);
+    }
+
+    static async setSession(id: string, sess: Session) {
+        await kv.set(['projects', id, 'sessions', ulid()], sess);
+    }
+
+    static async setDefaultProject(id: string): Promise<Result<null, string>> {
+        const project = await EtuStorage.getProjectById(id);
+        if (project.isNone()) return Err(`Project ${id} does not exist.`);
+        await kv.set(['projects', 'default'], id);
+
+        return Ok(null);
+    }
+}
