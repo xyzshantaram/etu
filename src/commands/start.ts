add655a3052d47fa23f2f0738d1e64c8614883b7
Siddharth Singh <me@shantaram.xyz>
2024-06-22 14:10:17 +0530

    create start subcommand

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,12 +1,22 @@
-import { Command } from "@commander";
-import { Maybe } from "../utils.ts";
+import { Command } from "@/commander";
+import { Maybe, getProjectId } from "../utils.ts";
+import { EtuStorage } from "../storage.ts";
+import { match } from "@/oxide";
 
 interface EStartOpts {
     id: string;
 }
 
-const action = (name: Maybe<string>, { id }: EStartOpts) => {
-    console.log(name, id);
+const action = async (name: Maybe<string>, { id }: EStartOpts) => {
+    return match(await getProjectId(id), {
+        Err: (msg: string) => { throw new Error(msg) },
+        Ok: async (id: string) => {
+            await EtuStorage.putSession(id, {
+                name,
+                start: new Date().valueOf()
+            })
+        }
+    });
 }
 
 export const startClock = (cmd: Command) => {
