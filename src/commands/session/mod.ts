f82af5088ffc1e9da0d25a1948919552dd3403b5
Siddharth Singh <me@shantaram.xyz>
2024-06-22 17:18:31 +0530

    session module

---
+++ src/commands/session/mod.ts
@@ -0,0 +1,8 @@
+import { Command } from "@/commander";
+import { start } from "./start.ts";
+import { stop } from "./stop.ts";
+
+export const session = new Command('session')
+    .description('Edit and manage sessions.')
+    .addCommand(start)
+    .addCommand(stop);
