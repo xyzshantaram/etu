70f863c29345e28c32479c8ded6d97fcf4990ef2
Siddharth Singh <me@shantaram.xyz>
2024-07-07 15:02:10 +0530

    format

--- src/commands/log/mod.ts
+++ src/commands/log/mod.ts
@@ -126,8 +126,9 @@ function fmtSession(idx: string, sess: Session, indentWidth: number) {
             strings.push(pad`\n${indent}on ${y1}-${a1}-${d1} from ${h1}:${m1} to ${h2}:${m2}`);
         } else if (y1 === y2 && a1 === a2 && d1 !== d2) {
             strings.push(
-                pad`\n${indent}in ${y1}-${a1} from ${h1}:${m1} (${toEnglishIndex(d1)}) to ${h2}:${m2} (${toEnglishIndex(d2)
-                    })`,
+                pad`\n${indent}in ${y1}-${a1} from ${h1}:${m1} (${toEnglishIndex(d1)}) to ${h2}:${m2} (${
+                    toEnglishIndex(d2)
+                })`,
             );
         } else if (y1 === y2 && a1 !== a2 && d1 !== d2) {
             strings.push(pad`\n${indent}in ${y1} from ${a1}-${d1} ${h1}:${m1} to ${a2}-${d2} ${h2}:${m2}`);
